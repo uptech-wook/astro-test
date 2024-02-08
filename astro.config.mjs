5901be5023392f0d87c9ac993f3ae438c92cbac9
uptech-wook <wook@uptech.partners>
2024-02-08 16:59:01 +0900

    quick start end

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://friendly-griffin-f079a7.netlify.app",
+  integrations: [preact()]
+});
